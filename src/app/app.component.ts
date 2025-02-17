dc17772793f145552d5f3f459ace07ca0066ad35
naopeke <naopeke@protonmail.com>
2025-02-17 16:24:35 +0100

    prep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatIcon, RouterLink, MatToolbar,
+    MatIconButton,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, resource, signal } from '@angular/core';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { environment } from '../../environments/environment';
+import { Lesson } from '../models/lesson.model';
 
 @Component({
   selector: 'app-resource-demo',
-  imports: [],
+  imports: [MatProgressSpinner],
   templateUrl: './resource-demo.component.html',
   styleUrl: './resource-demo.component.scss'
 })
 export class ResourceDemoComponent {
+  env = environment;
 
+  search = signal<string>('');
+
+  lessons = signal<Lesson[]>([])
+
+  constructor() {
+
+    effect(() => {
+      console.log('searching lessons:', this.search() );
+    })
+  }
+
+  searchLessons(search: string) {
+    this.search.set(search);
+  }
+
+  reset() {
+
+  }
+
+  reload() {
+
+  }
 }
